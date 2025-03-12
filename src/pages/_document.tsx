2e880a5fa152321a55678612a30174e00f9dc971
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-03-12 20:28:22 +0900

    feat: initialize _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="ja">
+			<Head>
+				<title>tl-todo-app</title>
+				<meta name="description" content="Exam for Timeleap Inc." />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
