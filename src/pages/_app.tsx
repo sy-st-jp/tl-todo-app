91efdda8d4c63d2ff6268c662b96e0704ed9a1ac
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-03-13 14:55:49 +0900

    feat: initial setup of chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { Provider } from "@/app/provider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<Provider>
+			<Component {...pageProps} />
+		</Provider>
+	);
 }
