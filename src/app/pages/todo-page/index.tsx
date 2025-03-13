6fda4833d46e435256bc2559180bef1a49df9348
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-03-13 16:54:46 +0900

    feat: create a TodoPage

---
+++ src/app/pages/todo-page/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Box} from "@chakra-ui/react";
+import {Todo} from "@/features/todo";
+
+export const TodoPage = () => {
+    return (
+        <Box p="4">
+            <Todo />
+        </Box>
+    )
+};
