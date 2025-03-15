5a5e5b0a0f0c4e831616655e11bbc4574891e916
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-03-16 01:08:18 +0900

    refactor: add a type of fetcher response to generics

--- src/features/todo/hooks/useTodo/index.ts
+++ src/features/todo/hooks/useTodo/index.ts
@@ -1,8 +1,9 @@
 import useSWR from "swr";
 import {getTodos} from "../../api";
+import {GetTodosResponse} from "../../api/types";
 
 export const useTodo = () => {
-    const { data, error, isLoading } = useSWR("getTodos", getTodos)
+    const { data, error, isLoading } = useSWR<GetTodosResponse>("getTodos", getTodos)
     return {
         data,
         error,
