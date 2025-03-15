fa585c76ae04b8d6b388db8c8ac7fa8a602dff82
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-03-16 00:59:41 +0900

    refactor: remove unnecessary const

--- src/features/todo/api/index.ts
+++ src/features/todo/api/index.ts
@@ -1,5 +1,3 @@
-const todoFetcher = createFetchers(env.TODO_API_HOST)
-
 // TODO: api client の責務を分割する
 export const getTodos = async () => {
     const res = await todoFetcher.get<undefined, Response, Path>({
