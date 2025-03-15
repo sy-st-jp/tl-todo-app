945e69dba73ddb1b3be9b1dde8e65d61c9c6d93e
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-03-16 01:02:12 +0900

    refactor: move the implementation to a separate file

--- src/features/todo/api/index.ts
+++ src/features/todo/api/index.ts
@@ -1,7 +1,2 @@
-// TODO: api client の責務を分割する
-export const getTodos = async () => {
-    const res = await todoFetcher.get<undefined, Response, Path>({
-        path: "/api/todos",
-    })
-    return res.data
-}
+import {getTodos} from "./modules/getTodos";
+export {getTodos}
