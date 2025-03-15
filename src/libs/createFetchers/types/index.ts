2bc6625735388deacc161000477d8368c8264006
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-03-16 00:44:03 +0900

    refactor: extract the return type of CreateFetchers

--- src/libs/createFetchers/types/index.ts
+++ src/libs/createFetchers/types/index.ts
@@ -1,10 +1,12 @@
 import {AxiosHeaders} from "axios";
-import {Fetcher} from "../modules/createFetcher/types";
+import { Fetcher } from "../modules/createFetcher/types";
 
-export type CreateFetchers = (host: string, sharedHeaders?: AxiosHeaders) => {
+export type Fetchers = {
     get: Fetcher,
     post: Fetcher,
     put: Fetcher,
     delete: Fetcher,
     patch: Fetcher,
 }
+
+export type CreateFetchers = (host: string, sharedHeaders?: AxiosHeaders) => Fetchers
