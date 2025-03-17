a2f6afce43164ccb1dd9f7a92110dba3bdc78ab1
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-03-17 18:43:23 +0900

    feat: add fetcher of mock to switchFetcher

--- src/features/todo/api/modules/createTodo/modules/fetchTodo/index.ts
+++ src/features/todo/api/modules/createTodo/modules/fetchTodo/index.ts
@@ -1,7 +1,11 @@
 import {switchFetcher} from "@/libs/switchFetcher";
 import {fetcherForProduction} from "./modules/fetcherForProduction";
 import {CreateTodo} from "@/features/todo/api/types";
+import {fetcherForMock} from "@/features/todo/api/modules/createTodo/modules/fetchTodo/modules/featcherForMock";
 
 export const fetchTodo: CreateTodo = (request) => {
-    return switchFetcher(async() => { return await fetcherForProduction(request)});
+    return switchFetcher(
+        async() => await fetcherForProduction(request),
+        async() => await fetcherForMock(request)
+    );
 }
