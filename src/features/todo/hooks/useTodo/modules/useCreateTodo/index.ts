0df61da04edec13ba127d5a95b42a9f06bb760e1
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-03-17 18:22:01 +0900

    feat: add process of toast creation to createTodo

--- src/features/todo/hooks/useTodo/modules/useCreateTodo/index.ts
+++ src/features/todo/hooks/useTodo/modules/useCreateTodo/index.ts
@@ -4,6 +4,7 @@ import {createTodo} from "@/features/todo/api";
 import {Todo} from "@/features/todo/types/Todo";
 import {KeyedMutator} from "swr";
 import {useState} from "react";
+import {toast} from "@/components/ui/toater";
 
 export const useCreateTodo = (mutate: KeyedMutator<Todo[]>) => {
     const [errorMessage, setErrorMessage] = useState<string>()
@@ -12,9 +13,11 @@ export const useCreateTodo = (mutate: KeyedMutator<Todo[]>) => {
         return await createTodo(arg)
     }, {
         onSuccess: async () => {
+            toast.create("タスクを作成しました", undefined, "success")
             await mutate()
         },
         onError: (error) => {
+            toast.create("タスクの作成に失敗しました", "時間を置いて再度お試しください。解決しない場合は、こちらにお問い合わせください。(https://...)", "error")
             setErrorMessage(error.message)
         }
     })
