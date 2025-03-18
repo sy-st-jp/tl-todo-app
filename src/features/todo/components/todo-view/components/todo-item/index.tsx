5aade75a7d86175ab418fcd797fe052f1da21d12
Shun Yoshida <shun.yoshida.p@gmail.com>
2025-03-18 11:22:39 +0900

    feat: add a delete button to TodoItem

--- src/features/todo/components/todo-view/components/todo-item/index.tsx
+++ src/features/todo/components/todo-view/components/todo-item/index.tsx
@@ -3,6 +3,7 @@ import React, {ChangeEvent, FC} from "react";
 import {UpdateItemDialog} from "@/features/todo/components/todo-view/components/UpdateItemDialog";
 import {Todo} from "@/features/todo/types/Todo";
 import {UpdateConfig} from "@/features/todo/hooks/useTodo/modules/useUpdateTodo/type/UpdateConfig";
+import {WrappedButton} from "@/components/wrapped/chakra-ui/ui/button";
 
 type Props = {
     todo: Todo
@@ -56,6 +57,9 @@ export const TodoItem: FC<Props> = (props) => {
                 <input type="checkbox" checked={!!completed} onChange={handleCompleted}/>
             </WrappedTableCell>
             <UpdateItemDialog todo={todo} isLoading={updateTodo.isLoading} onClickUpdateButton={updateTodo.handler} error={updateTodo.error}/>
+            <WrappedButton variant="outline" size="sm" onClick={handleDelete}>
+                削除
+            </WrappedButton>
         </WrappedTableRow>
     )
 }
